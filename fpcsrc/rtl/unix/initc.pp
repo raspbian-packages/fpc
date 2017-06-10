f816aa6b0cab40c1b82b2e23e2e643b144825ab4
Pascal Packaging Team <pkg-pascal-devel@lists.alioth.debian.org>
2017-06-10 18:13:48 +0100

    kfreebsd-amd64_bootstrap
    Gbp-Pq: Name kfreebsd-amd64_bootstrap.patch

--- fpcsrc/rtl/unix/initc.pp
+++ fpcsrc/rtl/unix/initc.pp
@@ -60,7 +60,7 @@ function geterrnolocation: pcint; cdecl;external clib name '__errno';
 {$endif}
 
 {$if defined(FreeBSD) or defined(DragonFly)} // tested on x86
-function geterrnolocation: pcint; cdecl;external clib name '__error';
+function geterrnolocation: pcint; cdecl;external clib name '__errno_location';
 {$endif}
 
 {$ifdef OpenBSD} // tested on x86
