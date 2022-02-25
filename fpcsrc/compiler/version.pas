54a0e26ed603dc749ff0e696cc747f750797fff0
Pascal Packaging Team <pkg-pascal-devel@lists.alioth.debian.org>
2022-02-25 04:48:27 +0000

    version
    This patch adds Debian build version to compiler full version.
    
    
    Gbp-Pq: Name version.diff

--- fpcsrc/compiler/version.pas
+++ fpcsrc/compiler/version.pas
@@ -81,6 +81,9 @@ function full_version_string:string;
 
 implementation
 
+const
+  FullVersionString={$INCLUDE version.inc};
+
 function version_string:string;
 begin
   version_string := version_nr+'.'+release_nr+'.'+patch_nr;
@@ -89,11 +92,7 @@ end;
 
 function full_version_string:string;
 begin
-  full_version_string := version_nr+'.'+release_nr+'.'+patch_nr+minorpatch
-{$ifdef REVINC}
-  +'-r'+{$i revision.inc}
-{$endif REVINC}
-  ;
+  full_version_string := FullVersionString;
 end;
 
 end.
