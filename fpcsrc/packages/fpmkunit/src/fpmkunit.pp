fcc710dc5a33191aca96415fa8eaa29a9d42c6bb
Abou Al Montacir <abou.almontacir@sfr.fr>
2026-02-27 16:30:28 +0000

    Change path of unit files to make it support MA co-installation.
    Bug-Debian: http://bugs.debian.org/73368
    Last-Update: Wed, 15 Jan 2025 00:05:11 +0100
    
    (Closes: Bug#73368)
    This patch was modified to fix default value of FPCDIR on ppc64el causing test
    suite of Castle Game Engine to break on that target.
    
    Gbp-Pq: Name fix-units-path-to-be-multi-arch-safe.patch

--- fpcsrc/packages/fpmkunit/src/fpmkunit.pp
+++ fpcsrc/packages/fpmkunit/src/fpmkunit.pp
@@ -4565,6 +4565,9 @@ begin
   BinInstallDir:='';
   LibInstallDir:='';
   ExamplesInstallDir:='';
+  if GlobalUnitDir = '' then begin
+    GlobalUnitDir := FBaseInstallDir;
+  end;
 end;
 
 
@@ -4911,6 +4914,21 @@ end;
 ****************************************************************************}
 
 procedure TFPCDefaults.CompilerDefaults;
+const
+{$ifdef CPUARMHF}
+  gnu = 'gnueabihf';
+{$endif CPUARMHF}
+{$ifdef CPUARMEL}
+  gnu = 'gnueabi';
+{$endif CPUARMEL}
+{$ifndef CPUARM}
+  gnu = 'gnu';
+{$endif CPUARM}
+{$if defined(CPUPOWERPC) and defined(ENDIAN_LITTLE)}
+  FullTarget = {$I %FPCTARGETCPU%} + 'le-' + {$I %FPCTARGETOS%} + '-' + gnu;
+{$ELSE}
+  FullTarget = {$I %FPCTARGETCPU%} + '-' + {$I %FPCTARGETOS%} + '-' + gnu;
+{$endif defined(CPUPOWERPC) and defined(ENDIAN_LITTLE)}
 var
   BD : String;
 begin
@@ -4921,7 +4939,7 @@ begin
   BD:=FixPath(GetEnvironmentVariable('FPCDIR'), False);
   if BD='' then
     begin
-      BD:='/usr/local/lib/fpc/'+FCompilerVersion;
+      BD:='/usr/lib/' + LowerCase(FullTarget) + '/fpc/'+FCompilerVersion;
       if not DirectoryExists(BD) and
          DirectoryExists('/usr/lib/fpc/'+FCompilerVersion) then
         BD:='/usr/lib/fpc/'+FCompilerVersion;
